3cffa6e8fadd72ae5b21c33341c53b825e57eed7
Thales Gomes <thalexbr@yahoo.com.br>
2024-09-01 11:56:57 -0300

    Adjusting dataframe columns

--- utils/aws_functions.py
+++ utils/aws_functions.py
@@ -2,16 +2,18 @@ import boto3
 
 def upload_to_s3(file, bucket, path):
 
-    sts_client = boto3.client('sts')
-    response = sts_client.assume_role(
-        RoleArn='arn:aws:iam::338003274337:role/EC2forSSMRole',
-        RoleSessionName='Upload_to_S3_Session'
-    )
+    # sts_client = boto3.client('sts')
+    # response = sts_client.assume_role(
+    #     RoleArn='arn:aws:iam::338003274337:role/EC2forSSMRole',
+    #     RoleSessionName='Upload_to_S3_Session'
+    # )
 
-    credentials = response['Credentials']
-    client = boto3.client('s3',
-                        aws_access_key_id=credentials['AccessKeyId'],
-                        aws_secret_access_key=credentials['SecretAccessKey'],
-                        aws_session_token=credentials['SessionToken'])
+    # credentials = response['Credentials']
+    # client = boto3.client('s3',
+    #                     aws_access_key_id=credentials['AccessKeyId'],
+    #                     aws_secret_access_key=credentials['SecretAccessKey'],
+    #                     aws_session_token=credentials['SessionToken'])
+
+    client = boto3.client('s3')
 
     client.upload_file(file, bucket, f'{path}/{file}')
--- utils/transform.py
+++ utils/transform.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import pyarrow as pa
 import pyarrow.parquet as pq
+import yaml
 
 def csv_to_parquet(csv_file, parquet_file, options):
 
@@ -8,8 +9,45 @@ def csv_to_parquet(csv_file, parquet_file, options):
   # with open(csv_filename,'r') as csv_file:
   df = pd.read_csv(csv_file, **options)
 
+  df = df.rename(columns={'Setor': 'setor','Código': 'cod','Ação': 'acao','Tipo':'tipo' ,'Qtde. Teórica':'qtde_teorica','Part. (%)':'part_perc','Part. (%)Acum.':'part_perc_acum'})
+
   # Convert the DataFrame to a PyArrow Table
   table = pa.Table.from_pandas(df)
 
   # Write the table to a Parquet file
   pq.write_table(table, parquet_file)
+
+
+def excel_to_yaml(excel_file, yaml_file):
+  """Converts an Excel file to a YAML file, using the first column for levels and lists.
+
+  Args:
+    excel_file: Path to the Excel file.
+    yaml_file: Path to the output YAML file.
+  """
+
+  # Read the Excel file into a Pandas DataFrame
+  df = pd.read_excel(excel_file)
+
+  # Create a YAML dictionary
+  yaml_dict = {}
+
+  # Iterate over the rows of the DataFrame
+  for index, row in df.iterrows():
+    current_dict = yaml_dict
+
+    # Create nested dictionaries based on the first column (levels)
+    for level in row.dropna().tolist()[:-1]:
+      if level not in current_dict:
+        current_dict[level] = {}
+      current_dict = current_dict[level]
+
+    # Add the last value as a list item
+    last_value = row.dropna().tolist()[-1]
+    if last_value not in current_dict:
+      current_dict[last_value] = []
+    current_dict[last_value].append(row.tolist()[1:])
+
+  # Write the YAML dictionary to a file
+  with open(yaml_file, 'w') as f:
+    yaml.dump(yaml_dict, f, default_flow_style=False)
